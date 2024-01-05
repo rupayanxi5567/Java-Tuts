ca49e7b939104e9fbc15adc74f8bbcc32cf97eee
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-05 18:44:31 +0000

    escape sequence done

---
+++ Escape_sequence_4.java
@@ -0,0 +1,13 @@
+/**
+ * Escape_sequence_4
+ */
+public class Escape_sequence_4 {
+    public static void main(String[] args) {
+        System.out.println("Hello 'the fonty' "); // used single quote
+        System.out.println("Hello \"the fonty\" "); // used double quote
+        System.out.println("Hello \\the fonty\\ "); // used backslash
+        System.out.println("Hello \nthe fonty "); // used new line
+        System.out.println("Hello \tthe fonty "); // used tab
+        System.out.println("Hello\bthe fonty "); // used backspace
+    }
+}
---
+++ Escape_sequence_prac5.java
@@ -0,0 +1,9 @@
+/**
+ * Escape_sequence_prac5
+ */
+public class Escape_sequence_prac5 {
+
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
