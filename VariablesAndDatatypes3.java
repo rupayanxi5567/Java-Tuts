60cb0bf03956e1c6c21ce89bef331d8bf66ac586
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-05 16:53:28 +0000

    data types done

---
+++ VariablesAndDatatypes3.java
@@ -0,0 +1,33 @@
+/**
+ * VariablesAndDatatypes3
+ */
+public class VariablesAndDatatypes3 {
+
+    public static void main(String[] args) {
+        // play with numbers
+
+        int nums;
+        nums = 33;
+        System.out.println(nums);
+
+        // play with booleans
+
+        boolean isRight = false;
+        System.out.println(isRight);
+
+        // play with floats
+
+        float piNum = 3.44f;
+        System.out.println(piNum);
+
+        // play with doubles
+
+        double myDouble = 3.666768787;
+        System.out.println(myDouble);
+
+        // play with doubles
+
+        String myChar = "Good night Fontyy";
+        System.out.println(myChar);
+    }
+}
