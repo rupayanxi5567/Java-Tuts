429dc060f5b8bc42104790afe0cb1b764931b421
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-05 14:03:20 +0000

    1st and 2nd done

---
+++ first_code.java
@@ -0,0 +1,8 @@
+/**
+ * first_code
+ */
+public class first_code {
+    public static void main(String[] args) {
+        System.out.println("Subscribe to Vakti Gyans and fonty");
+    }
+}
---
+++ second_code.java
@@ -0,0 +1,14 @@
+/**
+ * second_code
+ */
+public class second_code {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
