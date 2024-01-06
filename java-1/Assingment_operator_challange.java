8c867457f02719d129a98d0b9fb0bbe0f4870c6a
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-06 11:41:16 +0000

    operators challange done

---
+++ java-1/Assingment_operator_challange.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+/**
+ * Assingment_operator_challange
+ */
+public class Assingment_operator_challange {
+
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+
+
+        // challange-1
+
+        // System.out.print("Enter the value of A: ");
+        // int a = input.nextInt();
+
+        // System.out.print("Enter the value of B: ");
+        // int b = input.nextInt();
+
+        // System.out.println("Sum of them is " + (a+b));
+        // System.out.println("Minus of them is " + (a-b));
+        // System.out.println("Multiply of them is " + (a*b));
+        // System.out.println("Division of them is " + (a/b));
+        // System.out.println("Mod of them is " + (a%b));
+
+        // challange-2
+
+        // System.out.print("Enter the value of X: ");
+        // float x = input.nextFloat();
+        // System.out.print("Enter the value of Y: ");
+        // float y = input.nextFloat();
+
+        // System.out.println("The Product of x and y is " + (x*y));
+
+
+
+        // challange-3
+
+        // System.out.println("Enter the value of A: ");
+        // int a = input.nextInt();
+
+        // System.out.println("Enter the value of B: ");
+        // int b = input.nextInt();
+
+        // System.out.println("Enter the value of C: ");
+        // int c = input.nextInt();
+
+        // System.out.println("Enter the value of D: ");
+        // int d = input.nextInt();
+
+        // System.out.println("The perimiter of the rect. is " + (a+b+c+d));
+
+
+        // challange-4
+
+        // System.out.println("Enter the value of height");
+        // int height = input.nextInt();
+
+        // System.out.println("Enter the value of base");
+        // int base = input.nextInt();
+
+        // int area = (base*height)/2;
+
+        // System.out.println("The area of the triangle is " + area );
+
+
+
+        // challange-5
+
+        // System.out.println("Enter the value of Principle: ");
+        // int p = input.nextInt();
+
+        // System.out.println("Enter the value of time in month");
+        // int t = input.nextInt();
+
+        // System.out.println("Enter the value of interest rate");
+        // int r = input.nextInt();
+
+        // float interest = (p*t*r)/100;
+
+        // System.out.println("The overall interest rate is " + interest);
+
+
+        // challange-6
+
+        // System.out.println("Enter the value of Principle: ");
+        // int p = input.nextInt();
+
+        // System.out.println("Enter the value of time in month");
+        // int t = input.nextInt();
+
+        // System.out.println("Enter the value of interest rate");
+        // int r = input.nextInt();
+
+        // float interest = p*t*(1+r/100)
+
+        // System.out.println("The overall interest rate is " + interest);
+
+
+        // challange-7
+
+        // System.out.println("Enter the temp in ferenheit");
+        // float f = input.nextFloat();
+
+        // float c = (f-32)*5/9;
+
+        // System.out.println("Temp in celcius is " + c);
+
+
+
+    }
+}
