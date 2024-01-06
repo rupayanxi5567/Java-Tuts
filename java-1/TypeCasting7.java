532da88e1fe74c68cb326b7add019c06b00c70cc
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-06 09:14:02 +0000

    type casting done

---
+++ java-1/TypeCasting7.java
@@ -0,0 +1,17 @@
+/**
+ * TypeCasting7
+ */
+public class TypeCasting7 {
+
+    public static void main(String[] args) {
+
+        float a = 44;
+        System.out.println(a);
+
+        int b = (int) 44.65 ;
+        System.out.println(b);
+
+        long num = (long) 43.6778;
+        System.out.println(num);
+    }
+}
