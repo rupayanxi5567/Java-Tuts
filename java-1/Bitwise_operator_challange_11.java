d8d19207fd363f7e1f66f381ee56d8a05b473846
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-07 15:03:46 +0000

    bitwise operator challange done

---
+++ java-1/Bitwise_operator_challange_11.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * Bitwise_operator_challange_11
+ */
+public class Bitwise_operator_challange_11 {
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+
+        // //Challange-1
+
+        // System.out.println("Enter the value of X: ");
+        // int X = inputs.nextInt();
+        // System.out.println("Enter the value of Y: ");
+        // int Y = inputs.nextInt();
+
+        // int result = X & Y;
+
+        // System.out.println("The and operator of X and Y is " + result);
+
+
+
+        // //Challange-2
+
+        // System.out.println("Enter the value of X: ");
+        // int X = inputs.nextInt();
+        // System.out.println("Enter the value of Y: ");
+        // int Y = inputs.nextInt();
+
+        // int result = X | Y;
+
+        // System.out.println("The OR operator of X and Y is " + result);
+
+
+        //Challange-3
+
+        // System.out.println("Enter the value of X: ");
+        // int X = inputs.nextInt();
+        // System.out.println("Enter the value of Y: ");
+        // int Y = inputs.nextInt();
+
+        // int result = X ^ Y;
+
+        // System.out.println("The XOR operator of X and Y is " + result);
+
+
+
+        //Challange-4
+
+        // System.out.println("Enter the value of X: ");
+        // int X = inputs.nextInt();
+
+
+        // int result = ~X ;
+
+        // System.out.println("The Complement operator of X and Y is " + result);
+
+
+
+
+
+    }
+}
