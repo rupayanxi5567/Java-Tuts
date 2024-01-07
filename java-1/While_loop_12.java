2ec07b06d6e39fe27cc3767e1b21bc3ea912c2d7
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-07 16:02:52 +0000

    while loop done

---
+++ java-1/While_loop_12.java
@@ -0,0 +1,26 @@
+/**
+ * While_loop_12
+ */
+public class While_loop_12 {
+    public static void main(String[] args) {
+
+        // prog-1
+
+        // int num = 1;
+        // while (num < 100001) {
+        //     System.out.println(num);
+        //     num++;
+
+
+        // prog-2
+
+        int num = 500;
+        while (num >= 200) {
+            System.out.println(num);
+            num--;
+        }
+
+
+
+        }
+    }
