68d558abea5f22a98b14d7dd3608cf1e16f81b8f
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-08 11:09:53 +0000

    table, sum, factorial done

---
+++ java-1/Basics_15.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * Basics_15
+ */
+public class Basics_15 {
+
+    public static void main(String[] args) {
+
+        // challange-1
+
+        // for (int i = 0; i < 21; i++) {
+        //     int x=78;
+        //     System.out.println("The table of " + x + " is " + x + "x" + i + " = " + x*i );
+
+            // challange-2
+
+            // int sum = 0; 
+            // for (int i = 1; i < 101; i++) {
+            //     if (i%2 == 1) {
+            //         sum += i ;
+                    
+            //     }
+            // }
+            // System.out.println("The sum is " + sum);
+
+            // challange-3 [ needs corrections ]
+
+                // public static long factorial(int num){
+
+                //     Scanner input = new Scanner(System.in);
+                //     System.out.println("Enter the number for factorial: ");
+                //     num = input.nextInt();
+
+                    
+                //     if (num < 3 ) {
+                //         return 1;
+                //     } else {
+                //         long fact = 1;
+                //         int i = 2;
+                //         while (i <= num ) {
+                //             fact *= i;
+                //             i++;
+                //         }
+                //     }
+                // }
+
+    }
+}
