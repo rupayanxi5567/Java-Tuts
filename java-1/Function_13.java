9f57b541c45bb7e8c2dd32509ea6a892c7b80e83
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2024-01-08 07:16:50 +0000

    functions done

---
+++ java-1/Function_13.java
@@ -0,0 +1,16 @@
+/**
+ * Function_13
+ */
+public class Function_13 {
+    public static void main(String[] args) {
+        greet_fonty();
+    }
+
+    public static void greet_fonty(){
+        System.out.println("Good night fontyy");
+        System.out.println("Now go to sleep");
+    }
+
+    
+
+}
